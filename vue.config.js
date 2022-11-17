db02cb9216b2e15410d39751257a842c9e6457ff
sugar <th14200143@163.com>
2022-11-17 16:17:51 +0800

    add yml script

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: sugar th14200143@163.com
+ * @Date: 2022-11-17 14:13:13
+ * @LastEditors: sugar th14200143@163.com
+ * @LastEditTime: 2022-11-17 15:58:54
+ * @FilePath: \github-vue-action-demo\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: "/github-vue-action-demo/",
   transpileDependencies: true,
 });
